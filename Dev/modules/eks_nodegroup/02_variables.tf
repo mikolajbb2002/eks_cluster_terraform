907616d1ca04b7c05c2101c81f225fbd4c4ade0c
Miki <mikolajbb2002@users.noreply.github.com>
2025-11-28 12:58:09 +0100

    feat(eks): Add module for eks nodegroup

---
+++ Dev/modules/eks_nodegroup/02_variables.tf
@@ -0,0 +1,64 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster to attach the node group to."
+  type        = string
+}
+
+variable "aws_eks_node_group" {
+  description = "Name of the managed node group."
+  type        = string
+}
+
+variable "node_group_desired_size" {
+  description = "Desired number of nodes for the group."
+  type        = number
+}
+
+variable "node_group_max_size" {
+  description = "Maximum number of nodes for the group."
+  type        = number
+}
+
+variable "node_group_min_size" {
+  description = "Minimum number of nodes for the group."
+  type        = number
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for the node group."
+  type        = string
+}
+
+variable "node_ami_type" {
+  description = "AMI type for the managed node group (e.g., AL2_x86_64)."
+  type        = string
+}
+
+variable "node_disk_size" {
+  description = "Root volume size (GiB) for nodes."
+  type        = number
+}
+
+variable "number_of_subnets" {
+  description = "How many subnets to create for the node group."
+  type        = number
+}
+
+variable "availability_zones" {
+  description = "List of availability zones used for subnets."
+  type        = list(string)
+}
+
+variable "subnet_cidrs" {
+  description = "List of CIDR blocks matching each subnet."
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC identifier to attach the subnets to."
+  type        = string
+}
+
+variable "nodegroup_iam_role" {
+  description = "IAM role name for the managed node group."
+  type        = string
+}
