d32ece259009cde2877cde674a69b9d3fca881f5
Miki <mikolajbb2002@users.noreply.github.com>
2025-12-01 09:10:21 +0100

    feat(dynamodb): Parametrize dynamodb file

--- Dev/tfstate-bucket/03_variables.tf
+++ Dev/tfstate-bucket/03_variables.tf
@@ -27,3 +27,29 @@ variable "pipeline_oidc_subjects" {
   description = "my repo"
   type        = string
   }
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for Terraform state locking"
+  type        = string
+}
+
+variable "billing_mode" {
+  description = "Billing mode for the DynamoDB table"
+  type        = string
+}
+
+variable "hash_key" {
+  description = "Hash key for the DynamoDB table"
+  type        = string
+}
+
+variable "dynamodb_attribute_name" {
+  description = "Attribute name for the DynamoDB table"
+  type        = string
+}
+
+variable "dynamodb_attribute_type" {
+  description = "Attribute type for the DynamoDB table"
+  type        = string
+}
+
